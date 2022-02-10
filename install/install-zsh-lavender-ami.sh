7e08e0b1eb388dd41de96597fe3669c1c10b3f4f
Tung Pun <htung.nht@gmail.com>
2022-02-10 10:09:30 +0700

    Update install-zsh-lavender-ami.sh

--- install/install-zsh-lavender-ami.sh
+++ install/install-zsh-lavender-ami.sh
@@ -13,4 +13,5 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 sed -i.bak 's/^plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
 cd ~
+lchsh
 zsh
